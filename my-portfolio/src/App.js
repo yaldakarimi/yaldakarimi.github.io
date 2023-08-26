58fb2eaba6863c8193b4cd0609319c19cbb3cf8f
yalda karimi <yalda.karimi88@gmail.com>
2023-08-26 14:13:57 +0200

    setup tailwind

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -3,8 +3,8 @@ import "./App.css";
 function App() {
 	return (
 		<div className="App">
-			<header className="App-header">
-				<h1>Hello! it's me!</h1>
+			<header>
+				<h1 className="text-red-500">Hello! it's me!</h1>
 			</header>
 		</div>
 	);
