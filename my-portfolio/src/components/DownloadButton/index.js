8441f0e964c4a99e42f59a8b9bc9b13280665e78
yalda karimi <yalda.karimi88@gmail.com>
2023-08-27 23:50:33 +0200

    fix case issue of cv file

--- my-portfolio/src/components/DownloadButton/index.js
+++ my-portfolio/src/components/DownloadButton/index.js
@@ -3,7 +3,7 @@ import React from "react";
 const DownloadButton = () => {
 	return (
 		<div className="bg-pink-700 w-fit text-white px-4 py-2 rounded-full text-sm md:text-base">
-			<a href="cv-2023.pdf" download="cv-2023.pdf" alt="cv">
+			<a href="CV-2023.pdf" download="CV-2023.pdf" alt="cv">
 				Download
 			</a>
 		</div>
