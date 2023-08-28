ae9ae5fee7259f832f40624619dea5fae2ee6646
yalda karimi <yalda.karimi88@gmail.com>
2023-08-28 15:55:52 +0200

    update cv

--- my-portfolio/src/components/DownloadButton/index.js
+++ my-portfolio/src/components/DownloadButton/index.js
@@ -3,7 +3,7 @@ import React from "react";
 const DownloadButton = () => {
 	return (
 		<div className="bg-pink-700 w-fit text-white px-4 py-2 rounded-full text-sm md:text-base">
-			<a href="CV-2023.pdf" download="CV-2023.pdf" alt="cv">
+			<a href="CV.pdf" download="CV.pdf" alt="cv">
 				Download
 			</a>
 		</div>
